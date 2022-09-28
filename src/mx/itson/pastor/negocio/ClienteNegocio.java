bbcae83b4bf62ee61f5ad3dc9415850273f5527f
alumnog <alumnog@S923_21.itson.edu.mx>
2022-09-28 08:23:43 -0700

    Git del proyecto

---
+++ src/mx/itson/pastor/negocio/ClienteNegocio.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.itson.pastor.negocio;
+
+import mx.itson.pastor.persistencia.ClienteDAO;
+
+/**
+ *
+ * @author alumnog
+ */
+public class ClienteNegocio {
+    
+    public static boolean guardar(String nombre, String direccion, String telefono, String email){
+        boolean resultado = false;
+        try {
+            resultado = ClienteDAO.guardar(nombre, direccion, telefono, email);
+        } catch (Exception ex) {
+            System.err.print(ex.getMessage());
+        }
+        
+        return resultado;
+    }
+}
---
+++ src/mx/itson/pastor/persistencia/Conexion.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mx.itson.pastor.persistencia;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author alumnog
+ */
+public class Conexion {
+
+    public static Connection obtener(){
+        Connection conexion = null;
+        try{
+            conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/pastor?user=root&password=");
+           } catch(Exception e){
+                System.err.print(e.getMessage());
+           }
+     
+     return conexion;
+    
+    }
+    
+}
